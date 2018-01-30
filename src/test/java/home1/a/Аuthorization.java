a85ae251d076a7f807f7178fb2aef2729de7dfb7
Nastya <aranika@users.noreply.github.com>
2018-01-30 12:03:34 +0300

    alp

--- src/test/java/home1/a/Аuthorization.java
+++ src/test/java/home1/a/Аuthorization.java
@@ -22,7 +22,7 @@ public class Аuthorization {
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS); 
 		driver.get("http://86.57.161.116:10008/#/login"); Thread.sleep(10000);
 		driver.manage().window().maximize();
-		
+
 		if (Profile.FindEl("input[name='userName']", driver)){
 			WebElement log=driver.findElement(By.cssSelector("input[name='userName']"));Thread.sleep(10000);
 			log.click(); 	        log.sendKeys(login);
@@ -34,7 +34,7 @@ public class Аuthorization {
 					sub.click(); 	  		
 					Thread.sleep(20000);
 					res = (driver.getCurrentUrl().toString().equals("http://86.57.161.116:10008/#/vacation"));
-					
+
 				}}}
 		else {
 			res=false;
@@ -45,9 +45,20 @@ public class Аuthorization {
 
 	}
 
-	public static String getRandomWord(int length) {
+	public static String getRandomWord(int length, String lang) {
 		StringBuilder sb = new StringBuilder(Math.max(length, 16));
-		String alphabet = "QWERTYUIOPLKJHGFDSZXCVNMЙЦУКЕНГШЩЗДЛОРПАВЫЯЧСМТЬЬБЮЖЭХЪqwertyuiop[]asdfghjkl;'zxcvbnm,.1234567890-=`йцукенгшщзхъэждлорпавыфячсмитьбю.!№;%:?)_Э\f";
+		String alphabet;
+		if(lang.equals("eng")) {
+			alphabet= "ABCDEFGHIJKLMNOPQRSTUVWXYZzyxwvutsrqponmlkjihgfedcba";}//52
+		else if(lang.equals("rus")) {
+			alphabet= "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";//66
+		}
+		else if(lang.equals("sym")) {
+			alphabet= "!@#$%^&*()_+|-=\\{}[]:\";'<>?,./";//32
+		}
+		else {	
+			alphabet= "ABCDEFGHIJKLMNOPQRSTUVWXYZzyxwvutsrqponmlkjihgfedcba1234567890абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ[];',./-=`.!№;%:?)_";
+		}
 		for (int i = 0; i < length; i++) { 
 			int random = (int) (alphabet.length()*(Math.random()));
 			char c = alphabet.charAt(random);
@@ -57,19 +68,27 @@ public class Аuthorization {
 	}
 
 	public static String[][] Acc() {
-		r=(int)(Math.random()*10+10);
+		r=(int)(Math.random()*10+17);
 		String[][]logPas=new String [r][2];
 		logPas[0][0]="kolia"; logPas[0][1]="empl";
 		logPas[1][0]="";logPas[1][1]="";
-		logPas[2][0]="12_12_1991@list.ru"; logPas[2][1]="";
-		logPas[3][0]="";logPas[3][1]="100k7273";
-		logPas[4][0]=logPas[0][0]; logPas[4][1]="dww";
-		logPas[5][0]="dwdwdw";logPas[5][1]=logPas[0][1];
-		logPas[6][0]="dwdwdw";logPas[6][1]="dwww";
+		logPas[2][0]="kolia"; logPas[2][1]="";
+		logPas[3][0]="";logPas[3][1]="empl";
+		logPas[4][0]=logPas[0][0]; logPas[4][1]=getRandomWord(52,"eng");
+		logPas[5][0]=getRandomWord(52,"eng");logPas[5][1]=logPas[0][1];
+		logPas[6][0]=getRandomWord(52,"eng");logPas[6][1]=getRandomWord(52,"eng");
 		logPas[7][0]=""+(Math.random()*100);logPas[7][1]=""+(Math.random()*100);
 		logPas[8][0]=logPas[0][1]; logPas[8][1]=logPas[0][0];
-		for(int i=9; i<r; i++) {
-			logPas[i][0]=getRandomWord((int)(Math.random()*50+3)); logPas[i][1]=getRandomWord((int)(Math.random()*50+3));		
+		logPas[9][0]=getRandomWord(52,"eng")+""+(Math.random()*100);logPas[9][1]=getRandomWord(52,"eng")+""+(Math.random()*100);
+		logPas[10][0]=getRandomWord(66,"rus");logPas[10][1]=getRandomWord(66,"rus");
+		logPas[11][0]=getRandomWord(66,"rus")+(Math.random()*100);logPas[11][1]=getRandomWord(66,"rus")+(Math.random()*100);
+		logPas[12][0]=getRandomWord(66,"rus")+getRandomWord(52,"eng");logPas[12][1]=getRandomWord(66,"rus")+getRandomWord(52,"eng");
+		logPas[13][0]=getRandomWord(32,"sym");logPas[13][1]=getRandomWord(32,"sym");
+		logPas[14][0]=" kolia"; logPas[14][1]=" empl";
+		logPas[15][0]="kolia "; logPas[15][1]="empl ";
+		logPas[16][0]="kol ia"; logPas[16][1]="em pl";
+		for(int i=17; i<r; i++) {
+			logPas[i][0]=getRandomWord((int)(Math.random()*50+3),"all"); logPas[i][1]=getRandomWord((int)(Math.random()*50+3),"all");		
 		}
 		return logPas;
 	}
@@ -81,7 +100,8 @@ public class Аuthorization {
 		String s="";
 		String a="";
 		for(int i=0; i<r;i++) {
-			a=" №"+i+": result is "+LogIn(logPas[i][0], logPas[i][1])+";";
+			a=" №"+(i+1)+": result is "+LogIn(logPas[i][0], logPas[i][1])+" for log: "+logPas[i][0]+", pas: "+logPas[i][1]+ ";";
+//			a=" №"+(i+1)+": result is for log: "+logPas[i][0]+", pas: "+logPas[i][1]+ ";";
 			s=s+a;
 			System.out.println(a);
 		}
